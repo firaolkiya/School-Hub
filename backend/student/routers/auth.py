f3231a6339050bbfbd7640aacea4a23e5bb218eb
firaolkiya <firaolkiya6@gmail.com>
2025-05-01 17:47:58 +0300

    error fixed

--- backend/student/routers/auth.py
+++ backend/student/routers/auth.py
@@ -4,6 +4,6 @@ router = APIRouter(
     prefix="/students"
 )
 
-@router.post("",status_code=status.HTTP_201_CREATED)
+@router.post("/",status_code=status.HTTP_201_CREATED)
 def register_student(student:RegisterStudent):
     return student
