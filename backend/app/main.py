1c7259a66f1704ef89342f947818283325424622
firaolkiya <firaolkiya6@gmail.com>
2025-05-01 17:41:06 +0300

    end point fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-
+from student.routers import auth
 app = FastAPI()
 
+app.include_router(auth.router)
 @app.get("/")
 def index():
     return {"Message":"School; management system"}
